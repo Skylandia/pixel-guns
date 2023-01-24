ab0e3de6124880d0a5e530d9b88c224c7c4935ac
Skylandia <xrealify@gmail.com>
2023-01-24 20:00:58 +1300

    Optimize entity collision detection

--- src/main/java/com/ultreon/mods/pixelguns/item/gun/GunHitscanHelper.java
+++ src/main/java/com/ultreon/mods/pixelguns/item/gun/GunHitscanHelper.java
@@ -1,32 +1,23 @@
 package com.ultreon.mods.pixelguns.item.gun;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.Pair;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.RaycastContext;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Optional;
 
 public class GunHitscanHelper {
 
-    private static long time = 0;
     public static HitResult getCollision(LivingEntity shooter, double maxDistance) {
 
         EntityHitResult entityHitResult = GunHitscanHelper.getEntityCollision(shooter, maxDistance);
         if (entityHitResult != null) return entityHitResult;
 
 
-        if (System.currentTimeMillis() - time > 200) {
-            time = System.currentTimeMillis();
-            return GunHitscanHelper.getBlockCollision(shooter, maxDistance > 32 ? 32 : maxDistance);
-        }
-        return null;
+        return GunHitscanHelper.getBlockCollision(shooter, maxDistance > 32 ? 32 : maxDistance);
     }
 
     public static BlockHitResult getBlockCollision(LivingEntity shooter, double maxDistance) {
@@ -37,33 +28,35 @@ public class GunHitscanHelper {
 
         return shooter.getWorld().raycast(new RaycastContext(origin, destination, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.ANY, shooter));
     }
+
+    private record CollisionInfo(LivingEntity entity, Vec3d hitLocation, double distance2) { }
+
     public static EntityHitResult getEntityCollision(LivingEntity shooter, double maxDistance) {
         Vec3d origin = shooter.getEyePos();
         Vec3d direction = shooter.getRotationVector().normalize();
         Vec3d destination = origin.add(direction.multiply(maxDistance));
 
-        List<Pair<Double, LivingEntity>> result = new ArrayList<>();
+       CollisionInfo closestCollision = null;
+
         // Get all LivingEntities in the box bound by the origin and the destination
         for (final LivingEntity entity : shooter.getWorld().getEntitiesByClass(LivingEntity.class, shooter.getBoundingBox().stretch(direction.multiply(maxDistance)), (e) -> true)) {
             // Casts a ray from origin to destination to see if any entitiy hitboxes intersect it
             Optional<Vec3d> entityCollisionLocation = entity.getBoundingBox().raycast(origin, destination);
             if (entityCollisionLocation.isPresent()) {
                 // Entity found, add it to the result arraylist to be sorted
-                double distance = origin.squaredDistanceTo(entityCollisionLocation.get());
-                result.add(new Pair<>(distance, entity));
+                double distance2 = origin.squaredDistanceTo(entityCollisionLocation.get());
+                if (closestCollision == null || closestCollision.distance2 > distance2) {
+                    closestCollision = new CollisionInfo(entity, entityCollisionLocation.get(), distance2);
+                }
             }
         }
 
-        // Sort the entities from closest to furthest
-        result.sort(Comparator.comparingDouble(Pair::getLeft));
-
-        // If the shooter's line of sight intersects the closest entity (and not a block), return the result
-        // Otherwise get the next closest entity and repeat
-        for (Pair<Double, LivingEntity> pair : result) {
-            if (shooter.canSee(pair.getRight())) return new EntityHitResult(pair.getRight());
+        if (closestCollision != null) {
+            if (shooter.world.raycast(new RaycastContext(origin, closestCollision.hitLocation, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, shooter)).getType() == net.minecraft.util.hit.HitResult.Type.MISS) {
+                return new EntityHitResult(closestCollision.entity, closestCollision.hitLocation);
+            }
         }
 
-        // If we ran out of entities to check, return null
         return null;
     }
 }
