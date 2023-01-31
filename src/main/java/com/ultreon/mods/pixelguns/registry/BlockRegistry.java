8e11685e2488733129e16b786adb967ac07681a6
Skylandia <xrealify@gmail.com>
2023-02-01 09:00:53 +1300

    Remove UFO_INTERIOR

--- src/main/java/com/ultreon/mods/pixelguns/registry/BlockRegistry.java
+++ src/main/java/com/ultreon/mods/pixelguns/registry/BlockRegistry.java
@@ -1,6 +1,5 @@
 package com.ultreon.mods.pixelguns.registry;
 
-import com.ultreon.mods.pixelguns.block.UfoInteriorBlock;
 import com.ultreon.mods.pixelguns.block.WorkshopBlock;
 import com.ultreon.mods.pixelguns.util.ResourcePath;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
@@ -12,8 +11,6 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.registry.Registry;
 
 public class BlockRegistry {
-
-    public static final Block UFO_INTERIOR = BlockRegistry.register("ufo_interior", new UfoInteriorBlock(FabricBlockSettings.of(Material.METAL)));
     public static final Block WORKSHOP = BlockRegistry.register("workshop", new WorkshopBlock(FabricBlockSettings.of(Material.WOOD).strength(2.5f).sounds(BlockSoundGroup.WOOD).nonOpaque()));
 
     public static void registerBlockRenderers() {
