edf9a4f7653d0f600b9cb681edff8f166c0d1709
Skylandia <xrealify@gmail.com>
2023-02-01 09:01:15 +1300

    Update MISC category icon

--- src/main/java/com/ultreon/mods/pixelguns/item/ModCreativeTab.java
+++ src/main/java/com/ultreon/mods/pixelguns/item/ModCreativeTab.java
@@ -7,6 +7,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class ModCreativeTab {
-    public static final ItemGroup MISC = FabricItemGroupBuilder.build(new Identifier("pixel_guns", "misc"), () -> new ItemStack(ItemRegistry.MAGNUM_REVOLVER_BLUEPRINT));
+    public static final ItemGroup MISC = FabricItemGroupBuilder.build(new Identifier("pixel_guns", "misc"), () -> new ItemStack(ItemRegistry.ARMORED_VEST));
     public static final ItemGroup WEAPONS = FabricItemGroupBuilder.build(new Identifier("pixel_guns", "guns"), () -> new ItemStack(ItemRegistry.MAGNUM_REVOLVER));
 }
