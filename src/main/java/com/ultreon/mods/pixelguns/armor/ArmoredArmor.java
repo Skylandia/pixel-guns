541f593f549edd59ba12e4a2e6b31042e9a04d85
Skylandia <xrealify@gmail.com>
2023-02-05 15:42:08 +1300

    Clean up Geckolib armor classes

--- src/main/java/com/ultreon/mods/pixelguns/armor/ArmoredArmor.java
+++ src/main/java/com/ultreon/mods/pixelguns/armor/ArmoredArmor.java
@@ -2,27 +2,12 @@ package com.ultreon.mods.pixelguns.armor;
 
 import com.ultreon.mods.pixelguns.item.ModCreativeTab;
 import com.ultreon.mods.pixelguns.registry.ArmorRegistry;
+
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorItem;
-import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.core.manager.AnimationData;
-import software.bernie.geckolib3.core.manager.AnimationFactory;
-import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class ArmoredArmor extends ArmorItem implements IAnimatable {
-	
-	private final AnimationFactory factory = GeckoLibUtil.createFactory(this);
-
+public class ArmoredArmor extends GeoArmorItem {
 	public ArmoredArmor(EquipmentSlot equipmentSlot) {
 		super(ArmorRegistry.ARMORED, equipmentSlot, new FabricItemSettings().group(ModCreativeTab.MISC));
-	}
-
-	@Override
-	public void registerControllers(AnimationData data) {}
-
-	@Override
-	public AnimationFactory getFactory() {
-		return this.factory;
 	}
 }
--- src/main/java/com/ultreon/mods/pixelguns/armor/HazardArmor.java
+++ src/main/java/com/ultreon/mods/pixelguns/armor/HazardArmor.java
@@ -2,27 +2,12 @@ package com.ultreon.mods.pixelguns.armor;
 
 import com.ultreon.mods.pixelguns.item.ModCreativeTab;
 import com.ultreon.mods.pixelguns.registry.ArmorRegistry;
+
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorItem;
-import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.core.manager.AnimationData;
-import software.bernie.geckolib3.core.manager.AnimationFactory;
-import software.bernie.geckolib3.util.GeckoLibUtil;
 
-public class HazardArmor extends ArmorItem implements IAnimatable {
-	
-	private final AnimationFactory factory = GeckoLibUtil.createFactory(this);
-
+public class HazardArmor extends GeoArmorItem {
 	public HazardArmor(EquipmentSlot slot) {
 		super(ArmorRegistry.HAZARD, slot, new FabricItemSettings().group(ModCreativeTab.MISC));
-	}
-
-	@Override
-	public void registerControllers(AnimationData data) {}
-
-	@Override
-	public AnimationFactory getFactory() {
-		return this.factory;
 	}
 }
